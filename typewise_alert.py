7ae00b683da070f6c381df64fac74f747cddeb53
Aruna Gopalakrishnan <Gopalakrishnan.Aruna@in.bosch.com>
2021-04-06 10:17:42 +0530

    Implemented strategic pattern

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,11 +1,13 @@
 cooling_types_ranges = {
-    "PASSIVE_COOLING": {'lowerLimit': 0, 'upperLimit': 35},
-    "MED_ACTIVE_COOLING": {'lowerLimit': 0, 'upperLimit': 40},
-    "HI_ACTIVE_COOLING": {'lowerLimit': 0, 'upperLimit': 45},
+    "PASSIVE_COOLING": {'lower_limit': 0, 'upper_limit': 35},
+    "MED_ACTIVE_COOLING": {'lower_limit': 0, 'upper_limit': 40},
+    "HI_ACTIVE_COOLING": {'lower_limit': 0, 'upper_limit': 45},
 }
 
-email_alert_message = {"TOO_LOW": "Temperature is too low. Requesting immediate action",
-                       "TOO_HIGH": "Temperature is too high. Requesting immediate action"
+email_alert_message = {"TOO_LOW": {'recipient': 'bmslowbreachalert@bosch.com',
+                                   'alert': "Temperature is too low. Requesting immediate action"},
+                       "TOO_HIGH": {'recipient': 'bmshighbreachalert@bosch.com',
+                                    'alert': "Temperature is too high. Requesting immediate action"}
                        }
 
 DEFAULT_HEADER_VALUE = 0xfeed
@@ -15,19 +17,19 @@ def check_and_alert(alert_target, battery_char, temperature_in_celsius):
     breach_type = \
         classify_temperature_breach(battery_char['coolingType'], temperature_in_celsius)
     if breach_type != "NORMAL":
-        alert_target_type[alert_target](breach_type)
+        return alert_target_type[alert_target](breach_type)
 
 
 def classify_temperature_breach(cooling_type, temperature_in_celsius):
     if is_input_valid(cooling_type, temperature_in_celsius):
-        lower_limit, upper_limit = cooling_types_ranges[cooling_type]
-        return infer_breach(temperature_in_celsius, lower_limit, upper_limit)
+        cooling_temp_range = cooling_types_ranges[cooling_type]
+        return infer_breach(temperature_in_celsius, cooling_temp_range['lower_limit'], cooling_temp_range['upper_limit'])
     else:
         return 'INVALID_INPUT'
 
 
 def is_input_valid(cooling_type, temperature_in_celsius):
-    if cooling_type in cooling_types_ranges.keys() and temperature_in_celsius is None:
+    if cooling_type in cooling_types_ranges.keys() and temperature_in_celsius is not None:
         return True
     return False
 
@@ -42,18 +44,18 @@ def infer_breach(value, lower_limit, upper_limit):
 
 def send_to_controller(breach_type):
     header = DEFAULT_HEADER_VALUE
-    print(f'{header}, {breach_type}')
+    print(f'{header}, {breach_type} \n \tController Activated')
+    return "CONTROLLER_ACTIVATED"
 
 
-def create_email(email_recipient):
-    def send_email(breach_type):
-        email_message = email_alert_message[breach_type]
-        email_data = f"Dear,\n{email_recipient}\n \t {email_message}"
-        print(email_data)
-    return send_email
+def send_email(breach_type):
+    email_message = email_alert_message[breach_type]
+    email_data = f"To {email_message['recipient']},\n \t Dear Technician, \n \t {email_message['alert']}"
+    print(email_data)
+    return "EMAIL_SENT"
 
 
 alert_target_type = {
-    'email': create_email("bmstechnician@bosch.com"),
+    'email': send_email,
     'controller': send_to_controller
 }
--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -9,6 +9,21 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(typewise_alert.infer_breach(120, 0, 45) == 'TOO_HIGH')
         self.assertTrue(typewise_alert.infer_breach(20, 0, 40) == 'NORMAL')
 
+    def test_checks_input_data_valid(self):
+        self.assertTrue(typewise_alert.is_input_valid('PASSIVE_COOLING', 120))
+        self.assertFalse(typewise_alert.is_input_valid(None, -20))
+        self.assertFalse(typewise_alert.is_input_valid(None, None))
+        self.assertFalse(typewise_alert.is_input_valid('PASSIVE_COOLING', None))
+        self.assertFalse(typewise_alert.is_input_valid('ULTRA_COOLING', 120))
+
+    def test_checks_classify_temperature_breach_for_cooling_types(self):
+        self.assertTrue(typewise_alert.classify_temperature_breach('coolingType', 120))
+
+    def test_yields_alert_for_breaches(self):
+        self.assertEqual(typewise_alert.check_and_alert('email', {'coolingType': 'PASSIVE_COOLING'}, 120), 'EMAIL_SENT')
+        self.assertEqual(typewise_alert.check_and_alert('email', {'coolingType': 'HI_ACTIVE_COOLING'}, -20), 'EMAIL_SENT')
+        self.assertEqual(typewise_alert.check_and_alert('controller', {'coolingType': 'MED_ACTIVE_COOLING'}, 90), 'CONTROLLER_ACTIVATED')
+
 
 if __name__ == '__main__':
     unittest.main()
